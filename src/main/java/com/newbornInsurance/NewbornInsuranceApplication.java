e2b2994cc1eae33e1b00e54752dfc03d40457eff
yingkeli <clarke@nlerecovery.com>
2025-01-07 12:27:57 +0800

    add

---
+++ src/main/java/com/newbornInsurance/NewbornInsuranceApplication.java
@@ -0,0 +1,13 @@
+
+package com.newbornInsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbornInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbornInsuranceApplication.class, args);
+    }
+}
